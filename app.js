83b386446b076e5bbf2bc7633c3e34ff266afdcf
Steven Laurino <sjlaurino@gmail.com>
2019-02-07 17:01:28 -0700

    both weeks working

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
-let week1 = {
-  mon: {
-    hours: 0,
+let payPeriod = {
+  week1: {
+    mon: {
+      hours: 0,
+    },
+    tue: {
+      hours: 0,
+    },
+    wed: {
+      hours: 0,
+    },
+    thur: {
+      hours: 0,
+    },
+    fri: {
+      hours: 0,
+    }
   },
-  tue: {
-    hours: 0,
-  },
-  wed: {
-    hours: 0,
-  },
-  thurs: {
-    hours: 0,
-  },
-  fri: {
-    hours: 0,
+  week2: {
+    mon: {
+      hours: 0,
+    },
+    tue: {
+      hours: 0,
+    },
+    wed: {
+      hours: 0,
+    },
+    thur: {
+      hours: 0,
+    },
+    fri: {
+      hours: 0,
+    }
   }
 }
-
 let hour = 0
 
 function addHour(day) {
-  let time = week1[day].hours += 1;
+  let time = payPeriod.week1[day].hours += 1;
+  document.getElementById(day + 'Counter').innerHTML = time;
   console.log(time);
   return time;
 }
 function subHour(day) {
-  let time = week1[day].hours -= 1;
+  let time = payPeriod.week1[day].hours -= 1;
+  document.getElementById(day + 'Counter').innerHTML = time.toString();
   console.log(time);
   return time;
 }
-
-let monCounter = week1[day].hours
+function addHour2(day) {
+  let time = payPeriod.week2[day].hours += 1;
+  document.getElementById(day + 'Counter2').innerHTML = time;
+  console.log(time);
+  return time;
+}
+function subHour2(day) {
+  let time = payPeriod.week2[day].hours -= 1;
+  document.getElementById(day + 'Counter2').innerHTML = time.toString();
+  console.log(time);
+  return time;
+}
